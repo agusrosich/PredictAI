860c09543bfcfc476dcd425e752c589ce736cd5b
agusrosich <151882650+agusrosich@users.noreply.github.com>
2024-11-12 17:58:36 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ import joblib
 import numpy as np
 import pandas as pd
 import json
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
 app = Flask(__name__)
 
