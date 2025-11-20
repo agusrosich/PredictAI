428e3682aba272eef85cd07a1eaff1a94fab7890
agusrosich <151882650+agusrosich@users.noreply.github.com>
2025-11-20 09:56:56 -0300

    gitgud

--- app.py
+++ app.py
@@ -138,4 +138,5 @@ def api_predict():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    # Debug desactivado para producción
+    app.run(host='0.0.0.0', port=port, debug=False)
